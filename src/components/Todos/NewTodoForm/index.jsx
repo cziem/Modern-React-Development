c9e87824cf042f7488fd354b060170c3e4aa751e
Favour George <phavorsparks@gmail.com>
2020-11-12 20:50:25 +0100

    Style the header

--- src/components/Todos/NewTodoForm/index.jsx
+++ src/components/Todos/NewTodoForm/index.jsx
@@ -5,15 +5,17 @@ const NewTodoForm = () => {
   const [inputValue, setInputValue] = React.useState("");
 
   return (
-    <div className="new-todo-form">
-      <input
-        type="text"
-        value={inputValue}
-        className="new-todo-input"
-        placeholder="Enter new todo"
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button className="new-todo-button">Create Todo</button>
+    <div className="wrapper">
+      <div className="new-todo-form">
+        <input
+          type="text"
+          value={inputValue}
+          className="new-todo-input"
+          placeholder="Enter new todo"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button className="new-todo-button">Create Todo</button>
+      </div>
     </div>
   );
 };
