206596589d69ea9528d04eff1af21ade781436ee
Favour George <phavorsparks@gmail.com>
2020-11-12 23:11:01 +0100

    redux persist config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
 import { configureStore } from "./store/store";
 import App from "./App";
 import "./App.css";
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
